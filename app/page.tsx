86199cf593b534b7cd1812175b78d5b9b79ad1ff
Icy-Bear <prakhar.aka.nb@gmail.com>
2025-08-11 11:16:55 +0530

    init the project

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-5 items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Better Auth Starter</h1>
+      <p className="text-lg p-5">
+        This is a project for Better Auth. It is a simple project that uses
+        Better Auth to authenticate users.
+      </p>
+
+      <div className="flex gap-2">
+        <Link href="/login">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/signup">
+          <Button>Signup</Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
